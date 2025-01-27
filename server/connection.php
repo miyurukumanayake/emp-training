f43e0ad209bfa73c79c6104b83edaca40dc19621
Miyuru Kumanayake <mskumanayake@gmail.com>
2025-01-27 12:49:09 +0530

    publish

--- server/connection.php
+++ server/connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "9315@119Vm";
 $dbname = "emp-training";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
